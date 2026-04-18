9f110467e6937c2ab79f8717ed08cb0b48fbcafd
june-blah <dponce0020@gmail.com>
2026-04-17 20:05:37 -0700

    fixed sorter.py

--- sorter.py
+++ sorter.py
@@ -7,17 +7,13 @@ class Sorter:
         # note 2022: for chunk in read_csv(..., chunksize=x) -> chunk.itertuples()
         self.csv = pd.read_csv(file_path, usecols=["yieldPerAcre"])
 
-    def sort_by_yield(self, n):
-        # sort for top n yield records.
-        # if record.yield > lowest_sorted_yield
-        #   search sorted for lowest
-        #   replace lowest found with record.yield
-        sorted = [0] * n
-        lowest_sorted_yield = 0
+    # returns [[record_id, yield_per_acre]]
+    def sort_by_yield(self, n) -> [[int, int]]:
+        sorted = [[0, 0] for _ in range(n)]
         for record in self.csv.itertuples():
-            if record.yieldPerAcre > lowest_sorted_yield:
-                for i, v in enumerate(sorted):
-                    if v < record.yieldPerAcre:
-                        sorted[i] = record.yieldPerAcre
-                        break
+            for sorted_record in sorted:
+                if sorted_record[1] < record.yieldPerAcre:
+                    sorted_record[0] = record.Index
+                    sorted_record[1] = record.yieldPerAcre
+                    break;
         return sorted
